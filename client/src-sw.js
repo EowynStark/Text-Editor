e844f018837da1156e851819c14d16a7e2ae30e6
EowynStark <andwellm89@gmail.com>
2024-03-08 15:55:37 -0600

    adding asset caching to src-sw file

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,4 +1,4 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { offlineFallback, warmStrategyCache, StaleWhileRevalidate } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
@@ -26,5 +26,17 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// asset caching
+const assetCache = new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+registerRoute(({ request }) => request.destination === 'script' || request.destination === 'style', assetCache);
